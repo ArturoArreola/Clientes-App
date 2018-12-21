4c9a5f911cba37d84ac005821ec748a791edfd85
ArturoArreola <r2r_1310@hotmail.com>
2018-12-20 22:19:21 -0600

    ::Angular::
    Directivas para ngFor y ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido Angular';
+  curso: string = 'Curso con angular 7';
+  alumno: string = 'Arturo Arreola';
 }
