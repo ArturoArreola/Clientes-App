a7be1af0c74a38b3d521bd52bdb0b871ad4b8694
Nuuptech <nuup-user1@localhost.localdomain>
2019-01-02 18:09:19 -0600

    Feat: 2-Enero-2019

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Keith', apellido: 'Garrett', email: 'keith_89@example.com', createAt: '13/04/1989'},
+  {id: 2, nombre: 'Gary', apellido: 'Banks', email: 'gary_banks@example.com', createAt: '05/11/1988'},
+  {id: 3, nombre: 'Jerry', apellido: 'Fisher', email: 'albert_86@example.com', createAt: '12/04/1986'},
+  {id: 4, nombre: 'Emma', apellido: 'Stephens', email: 'carolyn.boyd@example.com', createAt: '16/04/1996'},
+  {id: 5, nombre: 'Olivia', apellido: 'Dunn', email: 'rebecca.shaw@example.com', createAt: '02/06/1984'},
+  {id: 6, nombre: 'Tiffany', apellido: 'Gordon', email: 'karen85@example.com', createAt: '03/06/1985'},
+  {id: 7, nombre: 'Mark', apellido: 'Stephens', email: 'lauren_owens@example.com', createAt: '13/09/1991'},
+  {id: 8, nombre: 'George', apellido: 'Freeman', email: 'victoria89@example.com', createAt: '14/12/1989'},
+  {id: 9, nombre: 'Ryan', apellido: 'Aguilar', email: 'joseph-88@example.com', createAt: '14/07/1988'},
+  {id: 10, nombre: 'Nancy', apellido: 'Reynolds', email: 'nancy88@example.com', createAt: '09/01/1988'}
+];
